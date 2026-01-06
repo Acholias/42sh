ab64e6e28dedbebab096f1b0e2d8c57a4121dedf
Acholias <mugot.lucas@gmail.com>
2026-01-06 15:57:24 +0100

    Recreate keys.c for add all TERMINAL_KEY's

--- includes/keys.h
+++ includes/keys.h
@@ -6,7 +6,7 @@
 /*   By: lumugot <lumugot@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 18:43:50 by lumugot           #+#    #+#             */
-/*   Updated: 2026/01/06 11:56:52 by lumugot          ###   ########.fr       */
+/*   Updated: 2026/01/06 12:37:01 by lumugot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,41 @@ typedef enum e_key
 	KEY_TAB,
 	KEY_ESC,
 	KEY_CTRL_A,
-	KEY_CTRL_E,
-	KEY_CTRL_K,
-	KEY_CTRL_U,
-	KEY_CTRL_W,
+	KEY_CTRL_B,
 	KEY_CTRL_C,
 	KEY_CTRL_D,
+	KEY_CTRL_E,
+	KEY_CTRL_F,
+	KEY_CTRL_K,
+	KEY_CTRL_L,
+	KEY_CTRL_R,
+	KEY_CTRL_T,
+	KEY_CTRL_U,
+	KEY_CTRL_W,
+	KEY_CTRL_Y,
+	KEY_CTRL_Z,
+	KEY_CTRL_BACKSLASH,
 	KEY_ARROW_UP,
 	KEY_ARROW_DOWN,
 	KEY_ARROW_RIGHT,
 	KEY_ARROW_LEFT,
+	KEY_CTRL_ARROW_LEFT,
+	KEY_CTRL_ARROW_RIGHT,
 	KEY_HOME,
 	KEY_END,
 	KEY_DELETE,
 	KEY_PAGE_UP,
-	KEY_PAGE_DOWN
+	KEY_PAGE_DOWN,
+	KEY_ALT_B,
+	KEY_ALT_C,
+	KEY_ALT_D,
+	KEY_ALT_F,
+	KEY_ALT_L,
+	KEY_ALT_T,
+	KEY_ALT_U,
+	KEY_ALT_BACKSPACE,
+	KEY_ALT_LESS,
+	KEY_ALT_GREATER
 }	t_key;
 
 typedef struct s_key_result
--- srcs/term/keys.c
+++ srcs/term/keys.c
@@ -6,48 +6,115 @@
 /*   By: lumugot <lumugot@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 18:41:40 by lumugot           #+#    #+#             */
-/*   Updated: 2026/01/05 23:52:42 by lumugot          ###   ########.fr       */
+/*   Updated: 2026/01/06 15:56:48 by lumugot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/keys.h"
+#include <unistd.h>
+
+static t_key	parse_alt_key(unsigned char ch)
+{
+	if (ch == 'b')
+		return (KEY_ALT_B);
+	else if (ch == 'c')
+		return (KEY_ALT_C);
+	else if (ch == 'd')
+		return (KEY_ALT_D);
+	else if (ch == 'f')
+		return (KEY_ALT_F);
+	else if (ch == 'l')
+		return (KEY_ALT_L);
+	else if (ch == 't')
+		return (KEY_ALT_T);
+	else if (ch == 'u')
+		return (KEY_ALT_U);
+	else if (ch == '<')
+		return (KEY_ALT_LESS);
+	else if (ch == '>')
+		return (KEY_ALT_GREATER);
+	if (ch == 127 || ch == 8)
+		return (KEY_ALT_BACKSPACE);
+	return (KEY_UNKNOWN);
+}
+
+static t_key	parse_bracket_sequence(unsigned char *seq)
+{
+	if (seq[0] >= '0' && seq[0] <= '9')
+	{
+		if (read(STDIN_FILENO, &seq[1], 1) != 1)
+			return (KEY_ESC);
+		if (seq[1] == '~')
+		{
+			if (seq[0] == '1')
+				return (KEY_HOME);
+			if (seq[0] == '3')
+				return (KEY_DELETE);
+			if (seq[0] == '4')
+				return (KEY_END);
+			if (seq[0] == '5')
+				return (KEY_PAGE_UP);
+			if (seq[0] == '6')
+				return (KEY_PAGE_DOWN);
+		}
+		else if (seq[0] == '1' && seq[1] == ';')
+		{
+			if (read(STDIN_FILENO, &seq[2], 1) != 1)
+				return (KEY_ESC);
+			if (seq[2] == '5')
+			{
+				if (read(STDIN_FILENO, &seq[3], 1) != 1)
+					return (KEY_ESC);
+				if (seq[3] == 'D')
+					return (KEY_CTRL_ARROW_LEFT);
+				if (seq[3] == 'C')
+					return (KEY_CTRL_ARROW_RIGHT);
+			}
+		}
+		return (KEY_UNKNOWN);
+	}
+	else
+	{
+		if (seq[0] == 'A')
+			return (KEY_ARROW_UP);
+		if (seq[0] == 'B')
+			return (KEY_ARROW_DOWN);
+		if (seq[0] == 'C')
+			return (KEY_ARROW_RIGHT);
+		if (seq[0] == 'D')
+			return (KEY_ARROW_LEFT);
+		if (seq[0] == 'H')
+			return (KEY_HOME);
+		if (seq[0] == 'F')
+			return (KEY_END);
+	}
+	return (KEY_UNKNOWN);
+}
 
 static t_key	parse_escape(void)
 {
 	unsigned char	seq[8];
 
-	if (read(STDIN_FILENO, &seq[0], 1) != 1) return (KEY_ESC);
-	if (read(STDIN_FILENO, &seq[1], 1) != 1) return (KEY_ESC);
-
+	if (read(STDIN_FILENO, &seq[0], 1) != 1)
+		return (KEY_ESC);
+	if (seq[0] >= 'a' && seq[0] <= 'z')
+		return(parse_alt_key(seq[0]));
+	if (seq[0] == '<' || seq[0] == '>' || seq[0] == 127 || seq[0] == 8)
+		return (parse_alt_key(seq[0]));
 	if (seq[0] == '[')
 	{
-		if (seq[1] >= '0' && seq[1] <= '9')
-		{
-			if (read(STDIN_FILENO, &seq[2], 1) != 1) return (KEY_ESC);
-			if (seq[2] == '~')
-			{
-				if (seq[1] == '1') return (KEY_HOME);
-				if (seq[1] == '3') return (KEY_DELETE);
-				if (seq[1] == '4') return (KEY_END);
-				if (seq[1] == '5') return (KEY_PAGE_UP);
-				if (seq[1] == '6') return (KEY_PAGE_DOWN);
-			}
-			return (KEY_UNKNOWN);
-		}
-		else
-		{
-			if (seq[1] == 'A') return (KEY_ARROW_UP);
-			if (seq[1] == 'B') return (KEY_ARROW_DOWN);
-			if (seq[1] == 'C') return (KEY_ARROW_RIGHT);
-			if (seq[1] == 'D') return (KEY_ARROW_LEFT);
-			if (seq[1] == 'H') return (KEY_HOME);
-			if (seq[1] == 'F') return (KEY_END);
-		}
+		if (read(STDIN_FILENO, &seq[1], 1) != 1)
+			return (KEY_ESC);
+		return (parse_bracket_sequence(&seq[1]));
 	}
 	else if (seq[0] == 'O')
 	{
-		if (seq[1] == 'H') return (KEY_HOME);
-		if (seq[1] == 'F') return (KEY_END);
+		if (read(STDIN_FILENO, &seq[1], 1) != 1)
+			return (KEY_ESC);
+		if (seq[1] == 'H')
+			return (KEY_HOME);
+		if (seq[1] == 'F')
+			return (KEY_END);
 	}
 	return (KEY_UNKNOWN);
 }
@@ -77,19 +144,36 @@ t_key_result	get_key(void)
 		res.key = KEY_TAB;
 	else if (ch == 1)
 		res.key = KEY_CTRL_A;
+	else if (ch == 2)
+		res.key = KEY_CTRL_B;
+	else if (ch == 3)
+		res.key = KEY_CTRL_C;
+	else if (ch == 4)
+		res.key = KEY_CTRL_D;
 	else if (ch == 5)
 		res.key = KEY_CTRL_E;
+	else if (ch == 6)
+		res.key = KEY_CTRL_F;
 	else if (ch == 11)
 		res.key = KEY_CTRL_K;
+	else if (ch == 12)
+		res.key = KEY_CTRL_L;
+	else if (ch == 18)
+		res.key = KEY_CTRL_R;
+	else if (ch == 20)
+		res.key = KEY_CTRL_T;
 	else if (ch == 21)
 		res.key = KEY_CTRL_U;
 	else if (ch == 23)
 		res.key = KEY_CTRL_W;
-	else if (ch == 4)
-		res.key = KEY_CTRL_D;
-	else if (ch == 3)
-		res.key = KEY_CTRL_C;
+	else if (ch == 25)
+		res.key = KEY_CTRL_Y;
+	else if (ch == 26)
+		res.key = KEY_CTRL_Z;
+	else if (ch == 28)
+		res.key = KEY_CTRL_BACKSLASH;
 	else
 		res.character = ch;
+	
 	return (res);
 }
