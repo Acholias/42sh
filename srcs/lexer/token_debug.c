14718c7ff0f8b9efed2b53a625d4138b0f49179d
Acholias <mugot.lucas@gmail.com>
2026-01-07 19:11:13 +0100

    fix token print and lexer base is ok, token wqs create and free before quit the shell

--- srcs/lexer/token_debug.c
+++ srcs/lexer/token_debug.c
@@ -6,7 +6,7 @@
 /*   By: lumugot <lumugot@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 18:20:00 by lumugot           #+#    #+#             */
-/*   Updated: 2026/01/07 18:20:00 by lumugot          ###   ########.fr       */
+/*   Updated: 2026/01/07 18:52:08 by lumugot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 #ifdef DEBUG
 
-# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
 
 # define COLOR_RESET   "\033[0m"
 # define COLOR_BOLD    "\033[1m"
@@ -92,30 +93,65 @@ static const char	*get_token_color(t_token_type type)
 
 void	token_print_one(t_token *token)
 {
+	const char	*type_name;
+	const char	*color;
+
 	if (!token)
 		return ;
-	printf("%s[%-16s]%s ", get_token_color(token->type),
-		get_token_type_name(token->type), COLOR_RESET);
+	color = get_token_color(token->type);
+	type_name = get_token_type_name(token->type);
+	write(STDOUT_FILENO, color, strlen(color));
+	write(STDOUT_FILENO, "[", 1);
+	write(STDOUT_FILENO, type_name, strlen(type_name));
+	write(STDOUT_FILENO, "]", 1);
+	write(STDOUT_FILENO, COLOR_RESET, strlen(COLOR_RESET));
+	write(STDOUT_FILENO, " ", 1);
 	if (token->value)
-		printf("'%s%s%s'", COLOR_BOLD, token->value, COLOR_RESET);
+	{
+		write(STDOUT_FILENO, "'", 1);
+		write(STDOUT_FILENO, COLOR_BOLD, strlen(COLOR_BOLD));
+		write(STDOUT_FILENO, token->value, strlen(token->value));
+		write(STDOUT_FILENO, COLOR_RESET, strlen(COLOR_RESET));
+		write(STDOUT_FILENO, "'", 1);
+	}
 	else
-		printf("(null)");
-	printf("\n");
+		write(STDOUT_FILENO, "(null)", 6);
+	write(STDOUT_FILENO, "\n", 1);
+}
+
+static void	put_number(int n)
+{
+	char	c;
+
+	if (n >= 10)
+		put_number(n / 10);
+	c = (n % 10) + '0';
+	write(STDOUT_FILENO, &c, 1);
 }
 
 void	token_print_all(t_token *tokens)
 {
 	int	i;
 
-	printf("\n%s=== TOKENS DEBUG ===%s\n", COLOR_BOLD, COLOR_RESET);
+	write(STDOUT_FILENO, "\r\n", 2);
+	write(STDOUT_FILENO, COLOR_BOLD, strlen(COLOR_BOLD));
+	write(STDOUT_FILENO, "=== TOKENS DEBUG ===", 20);
+	write(STDOUT_FILENO, COLOR_RESET, strlen(COLOR_RESET));
+	write(STDOUT_FILENO, "\n", 1);
 	i = 0;
 	while (tokens)
 	{
-		printf("%s[%d]%s ", COLOR_CYAN, i++, COLOR_RESET);
+		write(STDOUT_FILENO, "\r[", 2);
+		put_number(i++);
+		write(STDOUT_FILENO, "] -> ", 5);
 		token_print_one(tokens);
 		tokens = tokens->next;
 	}
-	printf("%s====================%s\n\n", COLOR_BOLD, COLOR_RESET);
+	write(STDOUT_FILENO, COLOR_BOLD, strlen(COLOR_BOLD));
+	write(STDOUT_FILENO, "\r", 1);
+	write(STDOUT_FILENO, "====================", 20);
+	write(STDOUT_FILENO, COLOR_RESET, strlen(COLOR_RESET));
+	write(STDOUT_FILENO, "\n\n", 2);
 }
 
 #endif
