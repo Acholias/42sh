95f7cae005662c5f46f1eca7a95f2439c2985a6c
Acholias <mugot.lucas@gmail.com>
2026-01-09 19:31:54 +0100

    create functions for redirs and create them before implent them in ast_parser

--- srcs/parser/free_parser.c
+++ srcs/parser/free_parser.c
@@ -6,7 +6,7 @@
 /*   By: lumugot <lumugot@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 19:11:08 by lumugot           #+#    #+#             */
-/*   Updated: 2026/01/09 19:12:44 by lumugot          ###   ########.fr       */
+/*   Updated: 2026/01/09 19:30:10 by lumugot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,35 @@ void	simple_cmd_free(t_simple_cmd *cmd)
 		free(cmd->argv);
 	}
 	free(cmd);
+}
+
+void	redir_free(t_redir *redirs)
+{
+	t_redir	*next;
+
+	while (redirs)
+	{
+		next = redirs->next;
+		free(redirs->file);
+		free(redirs->heredoc);
+		free(redirs);
+		redirs = next;
+	}
+}
+
+void	free_ast(t_ast_node *node)
+{
+	if (!node)
+		return ;
+	if (node->cmd)
+		simple_cmd_free(node->cmd);
+	if (node->redirs)
+		redir_free(node->redirs);
+	if (node->left)
+		free_ast(node->left);
+	if (node->right)
+		free_ast(node->right);
+	if (node->child)
+		free_ast(node->child);
+	free(node);
 }
