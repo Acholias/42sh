0e86d249dcd3f4263fe684856b87e5fe3c70baf1
Acholias <mugot.lucas@gmail.com>
2026-03-30 00:44:01 +0200

    Fininish to code all impl for history but not insert in read_line for store command and don't test them. I commit before insert and test all imlp

--- src/readline/history.rs
+++ src/readline/history.rs
@@ -29,4 +29,84 @@ impl    History
         let home = std::env::var("HOME").unwrap_or_else(|_| String::from("/tmp"));
         PathBuf::from(home).join(".42sh_history");
     }
+
+    fn load(&mut self)
+    {
+        let path = Self::path();
+
+        if let Ok(file) = File::open(&path)
+        {
+            let reader = BufReader::new(file);
+            for line in reader.lines()
+            {
+                if let Ok(entry) = line
+                {
+                    if  !entry.is_empty()
+                    {
+                        self.entries.push(entry);
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn save(&self, entry: &str)
+    {
+        if entry.is_empty() { return; }
+
+        let path = self::path();
+
+        if let Ok(mut file) = OpenOptions::new().append(true).create(true).open(&path)
+        {
+            writeln!(file, "{}", entry).unwrap();
+        }
+    }
+
+    pub fn push(&mut self, entry: String)
+    {
+        if !entry.is_empty()
+        {
+            self.save(&entry);
+            self.entries.push(entry);
+            self.index = self.entries.len();
+        }
+    }
+
+    pub fn prev(&mut self, current: &[char]) -> Option<&str>
+    {
+        if self.index == self.entries.len()
+        {
+            self.current = current.iter().collect();
+        }
+        if self.index > 0
+        {
+            self.index -= 1;
+            Some(&self.entries[self.index])
+        }
+        else
+        {
+            None
+        }
+    }
+
+    pub fn next(&mut self) -> Option<&str>
+    {
+        if self.index < self.entries.len()
+        {
+            self.index += 1;
+        }
+        if self.index == self.entries.len()
+        {
+            Some(&self.current)
+        }
+        else
+        {
+            Some(&self.entries[self.index])
+        }
+    }
+
+    pub fn len(&self) -> usize
+    {
+        self.entries.len();
+    }
 }
